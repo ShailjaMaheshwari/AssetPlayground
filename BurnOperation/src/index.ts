b589e5ad3a747b4e6982743526ae4655bf503082
Shailja Maheshwari <shailja.maheshwari@valuelabs.com>
2023-03-15 18:30:03 +0530

    foramtting code

--- BurnOperation/src/index.ts
+++ BurnOperation/src/index.ts
@@ -1,10 +1,9 @@
-import { Server, ServerApi, Operation } from "@abx/js-kinesis-sdk" ;// "stellar-sdk";
-const server =new Server ("https://devnet.kinesisgroup.io");
+import { Server, ServerApi, Operation } from "@abx/js-kinesis-sdk"; // "stellar-sdk";
+const server = new Server("https://devnet.kinesisgroup.io");
 //const server = new Server("https://horizon-testnet.stellar.org");
 const custodianId = "GBGNSAQAU7HZLE7SPR6LCY3LSORD2TV3I2Y647LDUIE2CCKKOALOKEP6"; //"GAHYA5Z4UWR6Z552XNWVYXXLM6CIU5OV3CT5BB2OQ4A2U5UZ4V6T6K3V";
-const tx_hash = "hfNDHmFPRHrN/uSVBlwnSfYvNCwT3Or2tfutVumX6H8=";//"TEST"; //"dswHXagyk/Y7mjm+a04/kUGm08aYpubPGvzsmatwA6s=";
+const tx_hash = "hfNDHmFPRHrN/uSVBlwnSfYvNCwT3Or2tfutVumX6H8="; //"TEST"; //"dswHXagyk/Y7mjm+a04/kUGm08aYpubPGvzsmatwA6s=";
 const creationDate = new Date("2023-02-28T05:35:58Z"); //("2023-02-17 00:58:08 UTC");//  "created_at": "2023-02-28T05:35:58Z",
-
 
 async function hasBurnOperationAlreadyHappened(
   custodianId: string,
@@ -31,7 +30,7 @@ async function hasBurnOperationAlreadyHappened(
       ) {
         console.log("checkpoint 1=====");
         const tx = await record.transaction();
-        if (tx.memo === tx_hash) { 
+        if (tx.memo === tx_hash) {
           console.log("checkpoint 2 ---", tx.memo);
           burnOperationExists = true;
           break;
@@ -39,38 +38,37 @@ async function hasBurnOperationAlreadyHappened(
       }
     }
     if (burnOperationExists) break;
-      operations = await operations.next();
-    
+    operations = await operations.next();
   }
   console.log("found: " + burnOperationExists);
-    return burnOperationExists;
+  return burnOperationExists;
 }
 
 hasBurnOperationAlreadyHappened(custodianId, tx_hash, creationDate);
 
-  // while (operations.records.length) {
-  //   operations.records.forEach(async (record) => {
-  //     let burnOperationExists = false;
-  //     if (
-  //       new Date(record.created_at) >= creationDate &&
-  //       record.type === "payment" &&
-  //       record.asset_type === "native" &&
-  //       record.source_account === custodianId
-  //     ) {
-  //       const tx = await record.transaction();
-  //       if (tx.memo === tx_hash) {
-  //         console.log(" found memo", record.transaction_hash);
-  //         burnOperationExists = true;
-  //         //console.log("TRUE: ", tx.memo);
-  //         return burnOperationExists;
-  //       }
-  //     }
-  //   });
-  //   if (!burnOperationExists) operations = await operations.next();
-  // }
-  // return burnOperationExists;
+//==========end of code===============
+// while (operations.records.length) {
+//   operations.records.forEach(async (record) => {
+//     let burnOperationExists = false;
+//     if (
+//       new Date(record.created_at) >= creationDate &&
+//       record.type === "payment" &&
+//       record.asset_type === "native" &&
+//       record.source_account === custodianId
+//     ) {
+//       const tx = await record.transaction();
+//       if (tx.memo === tx_hash) {
+//         console.log(" found memo", record.transaction_hash);
+//         burnOperationExists = true;
+//         //console.log("TRUE: ", tx.memo);
+//         return burnOperationExists;
+//       }
+//     }
+//   });
+//   if (!burnOperationExists) operations = await operations.next();
+// }
+// return burnOperationExists;
 //}
-
 
 //====================================================================
 //=====================================================================
